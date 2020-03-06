c5020b4c6a98908cc57c7e408d78beb82b8105f7
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-06 12:03:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,7 +7,7 @@ def using_unshift (array, string)
 end
 
 def using_pop (artists)
-  artists = ["Frida Kahlo", "Pablo Picasso", "Antartica"]
+  artists = ["Frida Kahlo", "Pablo Picasso", "Antarctica"]
   artists.pop
 end
 
