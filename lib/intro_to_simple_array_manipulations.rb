8724b9efe325d67466fc069fdfeeca9397548546
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-06 12:30:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,7 +39,7 @@ def using_insert(list_of_programming_languages, another_language)
 end
 
 def using_uniq(haircuts)
-haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+  haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
     new_array = using_uniq(haircuts)
 
 
