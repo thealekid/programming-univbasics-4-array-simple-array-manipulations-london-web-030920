61db8f4049b5b81950a18bd0dd0e7d9ae3105ed1
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-06 12:25:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,4 +26,16 @@ def shift_with_args (ice_cream_brands)
   @brands_removed = @ice_cream_brands.shift(2)
 end
 
-def using_concat 
+def using_concat(my_favorite_things, more_favs)
+  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["mario kart", "flatiron school"]
+  @all_my_favs = @my_favorite_things.concat(more_favs)
+end
+
+
+
+
+
+
+
+
