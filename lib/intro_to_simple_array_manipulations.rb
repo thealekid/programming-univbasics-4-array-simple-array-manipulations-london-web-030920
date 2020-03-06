8cdbf4a91123e439360b3ca853522406ce30d35c
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-06 12:33:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,7 +28,7 @@ end
 
 def using_concat(my_favorite_things, more_favs)
   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["mario kart", "flatiron school"]
+  more_favs = ["sports cars", "flatiron school"]
   @all_my_favs = @my_favorite_things.concat(more_favs)
 end
 
