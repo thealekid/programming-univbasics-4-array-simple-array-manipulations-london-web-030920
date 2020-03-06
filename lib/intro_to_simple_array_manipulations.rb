0263e414ec7245e769f76405c9d09d6fae20108c
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-06 12:48:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -53,7 +53,7 @@ def using_delete(instructors)
   instructors.delete("Steven")
 end
 
-def using_delete_at(famous_robots, )
+def using_delete_at(famous_robots, integer)
    famous_robots = ["Johnny 5", "R2D2", "Robocop"]
    integer = 2
    
