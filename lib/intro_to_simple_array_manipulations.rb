cf3ead7638c023235d408cfd849966d78e7ed354
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-06 12:09:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,7 +6,7 @@ def using_unshift (array, string)
   array.unshift (string)
 end
 
-def using_pop (artists)
+def using_pop (continents)
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
   continents.pop
 end
