6f7dabf97e749b38d287527fc9e0595853bb9aae
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-06 12:44:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -48,7 +48,7 @@ def using_flatten(instruments)
   flat_array = instruments.using_flatten
 end
 
-def using_delete (instructors)
+def using_delete(instructors)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
   instructors.delete("Steven")
 end
