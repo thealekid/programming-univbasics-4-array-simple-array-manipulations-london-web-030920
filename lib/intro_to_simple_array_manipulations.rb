c511bd670ce379cc5f026e4c27e67de0ea61be24
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-06 12:22:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,3 +18,6 @@ end
 
 def using_shift (my_favorite_cities)
   @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  @im_so_over_this_city = @my_favorite_cities.shift
+end
+
